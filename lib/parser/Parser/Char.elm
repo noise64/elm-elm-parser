343fa667c8a0d5bb94aefa08aeafb011902bbf99
Dávid István Bíró <noise64@gmail.com>
2015-02-09 00:37:17 +0100

    renamed digit to digitAsInt; added digit with Char return; added letter; added alphaNum

--- lib/parser/Parser/Char.elm
+++ lib/parser/Parser/Char.elm
@@ -1,4 +1,4 @@
-module Parser.Char (digit, natural, integer, float, upper, lower, parenthesized, bracketed, braced) where
+module Parser.Char (digit, natural, integer, float, upper, lower, alphaNum, letter, parenthesized, bracketed, braced) where
 
 {-| Some parsers
 
@@ -12,13 +12,17 @@ import Parser (..)
 import List
 import List (..)
 
-{-| Parse a digit -}
-digit : Parser Char Int
-digit = (\x -> Char.toCode x - 48) `Parser.map` satisfy Char.isDigit
+{-| Parse a digit as Int -}
+digit : Parser Char Char
+digit = satisfy Char.isDigit
+
+{-| Parse a digit as Int -}
+digitAsInt : Parser Char Int
+digitAsInt = (\x -> Char.toCode x - 48) `Parser.map` satisfy Char.isDigit
 
 {-| Parse a natural number -}
 natural : Parser Char Int
-natural = foldl (\b a -> a * 10 + b) 0 `Parser.map` some digit
+natural = foldl (\b a -> a * 10 + b) 0 `Parser.map` some digitAsInt
 
 {-| Parse an integer -}
 integer : Parser Char Int
@@ -26,7 +30,7 @@ integer = (always (\x -> -x) `Parser.map` (symbol '-')) `optional` identity `and
 
 {-| Parse a float -}
 float : Parser Char Float
-float = (\i f -> toFloat i + 0.1 * foldr (\b a -> a / 10 + b) 0 (List.map toFloat f))  `Parser.map` integer <* symbol '.' `and` some digit
+float = (\i f -> toFloat i + 0.1 * foldr (\b a -> a / 10 + b) 0 (List.map toFloat f))  `Parser.map` integer <* symbol '.' `and` some digitAsInt
 
 {-| Parse a upper case character -}
 upper : Parser Char Char
@@ -35,6 +39,14 @@ upper = satisfy Char.isUpper
 {-| Parse a lower case character -}
 lower : Parser Char Char
 lower = satisfy Char.isLower
+
+{-| Parse a letter or digit |-}
+alphaNum : Parser Char Char
+alphaNum = letter `or` digit
+
+{-| Parse a letter (an upper case or lower case character) |-}
+letter : Parser Char Char
+letter = satisfy (\ch -> (Char.isUpper ch || Char.isLower ch))
 
 {-| Parse a parser between parentheses `(` and `)`-}
 parenthesized : Parser Char r -> Parser Char r
@@ -47,3 +59,4 @@ bracketed p = symbol '[' *> p <* symbol ']'
 {-| Parses a parser between braces `{` and `}`-}
 braced : Parser Char r -> Parser Char r
 braced p = symbol '{' *>  p <* symbol '}'
+
